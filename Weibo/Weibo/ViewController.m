b2f7031425dbec065ec2fb5d48113ad43c77f983
Fung <fung@itcast.cn>
2015-02-10 18:33:10 +0800

    vvv
    vvvvv

--- Weibo/Weibo/ViewController.m
+++ Weibo/Weibo/ViewController.m
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+//    /xxxxx
+
+    //添加xx
+        //添加xx
+
 }
 
 - (void)didReceiveMemoryWarning {
